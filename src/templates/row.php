59890a7e6ebe542e98c3cc5a7e10391479bd030d
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-03-15 13:24:45 +0100

    handle full width row

--- src/templates/row.php
+++ src/templates/row.php
@@ -1,12 +1,19 @@
 <?php
-$classes = array( 'row' );
-if ( array_key_exists( 'noGutters', $attributes ) ) {
-	array_push( $classes, 'no-gutters' );
-}
+$classes = array( 'bootstrap-blocks-row' );
 if ( array_key_exists( 'className', $attributes ) ) {
 	array_push( $classes, $attributes['className'] );
 }
+if ( array_key_exists( 'align', $attributes ) && 'full' === $attributes['align'] ) {
+	array_push( $classes, 'full-width' );
+}
+
+$rowClasses = array( 'row' );
+if ( array_key_exists( 'noGutters', $attributes ) ) {
+	array_push( $rowClasses, 'no-gutters' );
+}
 ?>
 <div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
-	<?php echo $content; ?>
+	<div class="<?php echo esc_attr( implode( ' ', $rowClasses ) ); ?>">
+		<?php echo $content; ?>
+	</div>
 </div>
