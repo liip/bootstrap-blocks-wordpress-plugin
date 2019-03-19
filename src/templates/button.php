10dd6a5d1b668b7a6e2a9b33e8f1941d1b52e7df
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-03-19 13:59:44 +0100

    implement possibility to hook into button classes

--- src/templates/button.php
+++ src/templates/button.php
@@ -1,16 +1,22 @@
 <?php
-$classes = array( 'btn' );
+$classes = array( 'bootstrap-blocks-button' );
+$btn_classes = array( 'btn' );
+
 if ( array_key_exists( 'className', $attributes ) ) {
-	array_push( $classes, $attributes['className'] );
+	array_push( $btn_classes, $attributes['className'] );
 }
 if ( array_key_exists( 'style', $attributes ) ) {
-	array_push( $classes, 'btn-' . $attributes['style'] );
+	array_push( $btn_classes, 'btn-' . $attributes['style'] );
 } else {
-	array_push( $classes, 'btn-primary' );
+	array_push( $btn_classes, 'btn-primary' );
 }
+
+$btn_classes = apply_filters( 'bootstrap_blocks_button_classes', $btn_classes, $attributes );
+
+$classes = apply_filters( 'bootstrap_blocks_button_wrapper_classes', $classes, $attributes );
 ?>
-<div class="bootstrap-blocks-button">
-	<a href="<?php echo esc_url( $attributes['url'] ); ?>" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+	<a href="<?php echo esc_url( $attributes['url'] ); ?>" class="<?php echo esc_attr( implode( ' ', $btn_classes ) ); ?>">
 		<?php echo esc_html( $attributes['text'] ); ?>
 	</a>
 </div>
