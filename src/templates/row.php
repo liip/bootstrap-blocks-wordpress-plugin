0e8f304b9d9bcfeae8b334c0b5ce293236f1d94a
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-03-19 13:30:28 +0100

    Revert "remove unused align on row block"
    This reverts commit 4b72fc73

--- src/templates/row.php
+++ src/templates/row.php
@@ -3,6 +3,9 @@ $classes = array( 'bootstrap-blocks-row' );
 if ( array_key_exists( 'className', $attributes ) ) {
 	array_push( $classes, $attributes['className'] );
 }
+if ( array_key_exists( 'align', $attributes ) && 'full' === $attributes['align'] ) {
+	array_push( $classes, 'full-width' );
+}
 
 $rowClasses = array( 'row' );
 if ( array_key_exists( 'noGutters', $attributes ) ) {
