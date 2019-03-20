1a91c1f44b2413319764d18c1aa64ad6b623d6ff
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-03-20 21:18:56 +0100

    implement withBlockEditContext hoc

---
+++ src/block-edit-context.js
@@ -0,0 +1,43 @@
+// Source: https://github.com/WordPress/gutenberg/blob/master/packages/block-editor/src/components/block-edit/context.js
+
+/**
+ * External dependencies
+ */
+import { noop } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+const { createContext } = wp.element;
+const { createHigherOrderComponent } = wp.compose;
+
+const { Consumer } = createContext( {
+	name: '',
+	isSelected: false,
+	focusedElement: null,
+	setFocusedElement: noop,
+	clientId: null,
+} );
+
+/**
+ * A Higher Order Component used to inject BlockEdit context to the
+ * wrapped component.
+ *
+ * @param {Function} mapContextToProps Function called on every context change,
+ *                                     expected to return object of props to
+ *                                     merge with the component's own props.
+ *
+ * @return {Component} Enhanced component with injected context as props.
+ */
+export const withBlockEditContext = ( mapContextToProps ) => createHigherOrderComponent( ( OriginalComponent ) => {
+	return ( props ) => (
+		<Consumer>
+			{ ( context ) => (
+				<OriginalComponent
+					{ ...props }
+					{ ...mapContextToProps( context, props ) }
+				/>
+			) }
+		</Consumer>
+	);
+}, 'withBlockEditContext' );
