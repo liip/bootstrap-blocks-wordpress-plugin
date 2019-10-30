1cd9cb13b771551ad0edb9e5f705dee4087e10c9
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-10-30 08:50:48 +0100

    update template version to 1.4.0

--- src/templates/column.php
+++ src/templates/column.php
@@ -5,7 +5,7 @@
  * This template can be overridden by copying it to theme/wp-bootstrap-blocks/column.php.
  *
  * @package wp-bootstrap-blocks/templates/column
- * @version 1.1.0
+ * @version 1.4.0
  */
 
 /**
