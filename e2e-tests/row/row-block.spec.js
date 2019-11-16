b205878f434320c239b19b4683f9816a5f978c60
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-11-16 22:30:29 +0100

    add e2e tests for row block

---
+++ e2e-tests/row/row-block.spec.js
@@ -0,0 +1,79 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	createNewPost,
+	searchForBlock,
+	getAllBlocks,
+	selectBlockByClientId,
+	getEditedPostContent,
+	clickBlockToolbarButton,
+} from '@wordpress/e2e-test-utils';
+import {
+	insertRowBlock,
+	selectRowBlock,
+} from './row-helper'
+
+describe( 'row block', () => {
+	beforeEach( async () => {
+		await createNewPost();
+	} );
+
+	it( 'Row block should be available', async () => {
+		await insertRowBlock();
+
+		// Check if row block was inserted
+		expect( await page.$( '[data-type="wp-bootstrap-blocks/row"]' ) ).not.toBeNull();
+		expect( await page.$$( '[data-type="wp-bootstrap-blocks/column"]' ) ).toHaveLength(2);
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+
+		expect( console ).toHaveWarned();
+	} );
+
+	it( 'Should be possible to change alignment', async () => {
+		expect( console ).toHaveWarned();
+
+		await insertRowBlock();
+		await selectRowBlock();
+
+		// Change horizontal alignment
+		await clickBlockToolbarButton( 'Change horizontal alignment of columns' );
+		const [ alignmentRightButton ] = await page.$x("//button[contains(., 'Align columns right')]");
+		await alignmentRightButton.click();
+		expect( await page.$( '[data-type="wp-bootstrap-blocks/row"][data-alignment="right"]' ) ).not.toBeNull();
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+
+		// Change vertical alignment
+		await clickBlockToolbarButton( 'Change vertical alignment of columns' );
+		const [ alignmentBottomButton ] = await page.$x("//button[contains(., 'Align columns bottom')]");
+		await alignmentBottomButton.click();
+		expect( await page.$( '[data-type="wp-bootstrap-blocks/row"][data-vertical-alignment="bottom"]' ) ).not.toBeNull();
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'Should be possible to change column layout', async () => {
+		expect( console ).toHaveWarned();
+
+		await insertRowBlock();
+		await selectRowBlock();
+
+		// Layout options should be visible
+		expect( await page.$$( '.wp-bootstrap-blocks-template-selector-button' ) ).toHaveLength( 5 );
+		expect( await page.$( '.wp-bootstrap-blocks-template-selector-button > button[aria-label="2 Columns (1:1)"].is-active' ) ).not.toBeNull();
+		expect( await page.$$( '[data-type="wp-bootstrap-blocks/column"][data-size-md="6"]' ) ).toHaveLength( 2 );
+
+		// Template should be applied
+		await page.click( '.wp-bootstrap-blocks-template-selector-button > button[aria-label="3 Columns (1:1:1)"]' );
+		await page.waitFor( 1000 );
+		expect( await page.$$( '[data-type="wp-bootstrap-blocks/column"][data-size-md="4"]' ) ).toHaveLength( 3 );
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+		await page.click( '.wp-bootstrap-blocks-template-selector-button > button[aria-label="2 Columns (2:1)"]' );
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+
+		// Custom template should add block list appender (shouldn't change current layout)
+		await page.click( '.wp-bootstrap-blocks-template-selector-button > button[aria-label="Custom"]' );
+		expect( await page.$( '.wp-block-wp-bootstrap-blocks-row > .block-editor-inner-blocks > .block-editor-block-list__layout > .block-list-appender' ) ).not.toBeNull();
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+} );
---
+++ e2e-tests/row/row-helper.js
@@ -0,0 +1,15 @@
+import {
+	searchForBlock,
+} from '@wordpress/e2e-test-utils';
+import {
+	selectBlockByName,
+} from '../helper';
+
+export const insertRowBlock = async () => {
+	await searchForBlock( 'Bootstrap Row' );
+	await page.click( 'button.editor-block-list-item-wp-bootstrap-blocks-row' );
+};
+
+export const selectRowBlock = async () => {
+	await selectBlockByName( 'wp-bootstrap-blocks/row' );
+};
