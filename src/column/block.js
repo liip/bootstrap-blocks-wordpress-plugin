8edf4421b6183e2a174773d5e475741b82dfa77c
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-11-18 21:03:49 +0100

    improve block keywords

--- src/column/block.js
+++ src/column/block.js
@@ -14,9 +14,10 @@ registerBlockType( 'wp-bootstrap-blocks/column', {
 	icon: 'menu', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'wp-bootstrap-blocks', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
-		__( 'Bootstrap Blocks', 'wp-bootstrap-blocks' ),
-		__( 'Bootstrap', 'wp-bootstrap-blocks' ),
 		__( 'Column', 'wp-bootstrap-blocks' ),
+		__( 'Bootstrap Column', 'wp-bootstrap-blocks' ),
+		__( 'Bootstrap', 'wp-bootstrap-blocks' ),
+		__( 'Bootstrap Blocks', 'wp-bootstrap-blocks' ),
 	],
 	parent: [ 'wp-bootstrap-blocks/row' ],
 
