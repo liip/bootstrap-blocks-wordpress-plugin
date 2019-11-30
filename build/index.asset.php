3f348d15406c54c84f071df15d14ef686038ea0a
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-11-30 18:21:53 +0100

    add deprecation warnings for deprecated filters

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '91bf3e072835e4fa7f1f260f8a9d44ea');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '07a09f6ef3fd9a78b4287ddce323417e');
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-/**
- * Gutenberg Blocks
- *
- * All blocks related JavaScript files should be imported here.
- * You can create a new block folder in this dir and include code
- * for that block here as well.
- *
- * All blocks should be included here since this is the file that
- * Webpack is compiling as the input file.
- */
-
+import './deprecations';
 import './custom-category-icon';
 import './container/block.js';
 import './column/block.js';
