d3f46fcf74f5aaea222260ea7a40038688b71fd0
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-01-21 09:35:22 +0100

    improve translation scripts

--- scripts/translations/compile_translation_files.sh
+++ scripts/translations/compile_translation_files.sh
@@ -1,4 +1,7 @@
 HERE=`dirname $0`
 ROOT="$HERE/../.."
-for file in `find "$ROOT/languages" -name "*.po"` ; do msgfmt -o ${file/.po/.mo} $file ; done
-php $ROOT/wp-cli.phar i18n make-json $ROOT/languages/wp-bootstrap-blocks-de_DE.po --no-purge
+for file in `find "$ROOT/languages" -name "*.po"`
+do
+  msgfmt -o ${file/.po/.mo} $file
+  php $ROOT/wp-cli.phar i18n make-json $file --no-purge
+done
--- scripts/translations/update_translation_files.sh
+++ scripts/translations/update_translation_files.sh
@@ -1,3 +1,6 @@
 HERE=`dirname $0`
 ROOT="$HERE/../.."
-msgmerge -U "$ROOT/languages/wp-bootstrap-blocks-de_DE.po" "$ROOT/languages/wp-bootstrap-blocks.pot"
+for file in `find "$ROOT/languages" -name "*.po"`
+do
+  msgmerge -U $file "$ROOT/languages/wp-bootstrap-blocks.pot"
+done
