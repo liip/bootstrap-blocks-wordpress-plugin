49c106fc94c0b5523ff7ca5701ffe034a78f7903
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-04-08 21:45:17 +0200

    fix column padding in WordPress >= 5.4

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '4734e6dde78549c31050e85125f776c2');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '97d57c9fde91c8243698dacf619a354c');
