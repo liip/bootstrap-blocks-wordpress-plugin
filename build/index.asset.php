0d3a2f4a37321123508fefff3bca1773ec053152
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-04-08 23:38:43 +0200

    fix usage of button-size scss variable

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '97d57c9fde91c8243698dacf619a354c');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '55f1fe700a808a0fd1eddc297cdb72fb');
