f91b50c890055da37f91fbffd99e6c4263eff462
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-04-11 20:58:46 +0200

    build assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '55f1fe700a808a0fd1eddc297cdb72fb');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '03a3883af8bb6ea879f6929ea77e478a');
