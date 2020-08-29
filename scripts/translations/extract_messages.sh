05403e1bd50d6ff259e892e3050d0b21ac75611f
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-08-29 22:32:13 +0200

    create scripts to create json file

--- scripts/translations/extract_messages.sh
+++ scripts/translations/extract_messages.sh
@@ -2,4 +2,10 @@
 
 HERE=`dirname $0`
 ROOT="$HERE/../.."
-php $ROOT/wp-cli.phar i18n make-pot --exclude="wordpress" . $ROOT/languages/wp-bootstrap-blocks.pot
+
+# Create non-minified build to be able to extract labels
+(cd $ROOT && NODE_ENV=development npm run build)
+
+php $ROOT/wp-cli.phar i18n make-pot --include="build,wp-bootstrap-blocks.php,src/*.php" . $ROOT/languages/wp-bootstrap-blocks.pot
+
+(cd $ROOT && npm run build)
