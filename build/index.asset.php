4b70c256ef9c8488c81959d87406df7349379e97
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-10-18 13:27:31 +0200

    compile assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'c499030b78c39e1139755bc9bd352e96');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'c162fdf6c684d8eb6a91356525cafdf8');
