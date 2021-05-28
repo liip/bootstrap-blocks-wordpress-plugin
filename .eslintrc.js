58eeb5ab4a454c27df3e46897f4c9a81750085c0
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-05-28 23:18:25 +0200

    only use cypress/recommended linting rules for cypress tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
 	root: true,
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended',
-		'plugin:cypress/recommended',
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	overrides: [
+		{
+			// Use cypress/recommended plugin for cypress tests.
+			files: [ 'cypress/**/*.js' ],
+			extends: [ 'plugin:cypress/recommended' ],
+		},
 	],
 	rules: {
 		'jest/expect-expect': [
