53c6742ca7b294b99815c059b41407d5d79955fa
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-06-13 20:47:01 +0200

    set column template version to 3.3.0

--- src/templates/column.php
+++ src/templates/column.php
@@ -5,7 +5,7 @@
  * This template can be overridden by copying it to theme/wp-bootstrap-blocks/column.php.
  *
  * @package wp-bootstrap-blocks/templates/column
- * @version 3.0.0
+ * @version 3.3.0
  */
 
 /**
