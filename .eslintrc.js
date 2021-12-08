dec75d6d3350a2c53ed8c78623057ba900afc42e
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-12-08 21:37:53 +0100

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
 	root: true,
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	parserOptions: {
+		requireConfigFile: false,
+		babelOptions: {
+			presets: [ require.resolve( '@wordpress/babel-preset-default' ) ],
+		},
+	},
 	overrides: [
 		{
 			// Use cypress/recommended plugin for cypress tests.
