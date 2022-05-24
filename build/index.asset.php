ae5665f3824f587d3995d841ab103be16808353b
Juerg Hunziker <juerg.hunziker@liip.ch>
2022-05-24 09:43:17 +0200

    rename blocks for better discoverability

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2a125809b525a250b9ebef8d56322b37');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '880a7b58237fe7651d7b3ff82836e11b');
