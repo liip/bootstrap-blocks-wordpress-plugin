edf940067ad6da0abe08b5f3d46233fa89a9681e
Célian <celian@nematis.com>
2022-08-30 17:01:44 +0200

    fix parentClientId setting on accordion-item

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '3aeab9fb428997c9cd55');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '16a3819d63309fa4f024');
