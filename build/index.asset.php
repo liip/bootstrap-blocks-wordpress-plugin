df0af86cc47f04c282a16e11d1d910c112d1b5bb
Juerg Hunziker <juerg.hunziker@liip.ch>
2023-11-12 09:30:13 +0100

    fix insertRowBlock cypress command

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'd3457b9a3c8cbcffa04f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'd05b0de3c3da8ad0e3a7');
