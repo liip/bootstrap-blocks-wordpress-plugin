a42e90dd4dafbd8ff8264ccfed449e834c6286a6
Juerg Hunziker <juerg.hunziker@liip.ch>
2023-11-19 22:11:58 +0100

    build assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'd05b0de3c3da8ad0e3a7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => '711b66ceb1a605b9118c');
